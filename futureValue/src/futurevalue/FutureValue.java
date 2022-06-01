c9cea7ff8ca6dc2d9ef9cf745ed728da185606f3
Kevin Bell <bellKevin@pm.me>
2022-06-01 00:28:56 +0000

    Update FutureValue.java

--- futureValue/src/futurevalue/FutureValue.java
+++ futureValue/src/futurevalue/FutureValue.java
@@ -1,23 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package futurevalue;
 
 import java.util.Scanner;
 
-/**
- *
- * @author 4800590195
- */
 public class FutureValue {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.print("Future Value Calculator by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         double interestRate;
